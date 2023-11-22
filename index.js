896b2be18dccd475ae8e0bd14c6503426e5f7802
mauriciomouraa <mauricioalb.moura@gmail.com>
2023-11-22 11:47:05 -0300

    Desafio de Projeto 2 - Curso DEV DIO

---
+++ index.js
@@ -0,0 +1,48 @@
+// Desafio de Projeto
+
+// Recebe a Qtde de Vitórias e Derrotas
+let wins = gameHero(2000,1900);
+
+// Verifica o Saldo de Vitórias
+let nivelHero = nivel(wins)
+
+// Altera texto do saldo
+if (wins <= 0){
+     // Saída
+     console.log("O Herói não possui saldo de vitórias e está no nível " + nivelHero + "!");
+     
+} else{
+	// Saída
+    console.log("O Herói tem saldo de " + wins + " vitórias e está no nível " + nivelHero + "!");
+    
+}
+
+// Função para verificar saldo de vitórias
+function gameHero (winHero, losHero){
+    let rank = winHero - losHero;
+    return rank;
+}
+
+// Função para verificar o Rank
+function nivel (wins){
+	let nivel;
+    if (wins <= 10){
+    	nivel = "Ferro";
+    } else if (wins > 10 && wins <= 20 ){
+    	nivel = "Bronze";
+    } else if (wins > 20 && wins <= 50 ){
+    	nivel = "Prata";
+    } else if (wins > 50 && wins <= 80 ){
+    	nivel = "Ouro";
+    } else if (wins > 80 && wins <= 90 ){
+    	nivel = "Diamante";
+    } else if (wins > 90 && wins <= 100 ){
+    	nivel = "Lendário";
+    } else if (wins >= 101 ){
+    	nivel = "Imortal";
+    } else{
+    	nivel = "Não Encontrado"
+    }
+    
+    return nivel;
+} 
